d51df72a718b269e2a6840f8f6af82862f913c2c
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-06-27 03:24:11 +0600

    mail name change

--- src/Page/Sheard/Nabvar.js
+++ src/Page/Sheard/Nabvar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Nabvar = () => {
     return (
         <div className='sticky top-0 bg-slate-800 dark:bg-slate-800 px-12 py-6 flex justify-center text-gray-300 rounded-b-lg'>
-            <div className='flex text-xs font-semibold font-serif gap-5'>
+            <div className='flex text-xs font-semibold font-serif sm:gap-1 lg:gap-5'>
                 <Link to='/'>HOME</Link>
                 <Link to='about'>ABOUT</Link>
                 <Link to='/blogs'>BLOGS</Link>
